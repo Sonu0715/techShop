aa0b86675114670c1a577f443f280e79d7f74bcb
Sonu1507 <rsonu150794@gmail.com>
2021-10-06 20:48:38 +0530

    react-bootstrap setup,header n footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-function App() {
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
+const App = () => {
   return (
     <>
-      <h3>hello</h3>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h3>hello</h3>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
