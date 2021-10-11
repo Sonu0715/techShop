a89f201ee65700e9f61fc08f072884f31b4f42cc
Sonu1507 <rsonu150794@gmail.com>
2021-10-11 19:53:05 +0530

    Add to cart functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,7 +19,7 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   : []
 
 const initialState = {
-  cart: { cartItems: 'hello' },
+  cart: { cartItems: cartItemsFromStorage },
 }
 
 const middleware = [thunk]
