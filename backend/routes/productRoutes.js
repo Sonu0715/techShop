c5f5296d0646538894adc0017f5f4df74568d1d4
Sonu1507 <rsonu150794@gmail.com>
2021-10-12 20:05:45 +0530

    Cleanup by using controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,15 @@
 const express = require('express')
 const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
+const {
+  getProducts,
+  getProductById,
+} = require('../contollers/productContoller')
 
 const router = express.Router()
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    // res.status(401)
-    // throw new Error('not authorized')
-    res.json(products)
-  })
-)
+router.route('/').get(getProducts)
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product Not Found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 module.exports = router
